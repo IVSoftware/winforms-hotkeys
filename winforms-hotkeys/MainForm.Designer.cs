fb38fb927b1008566cf373452da60c0b92c4924b
Tom Gregor <tgregor@ivsoftware.com>
2024-10-03 11:16:33 -0600

    Add project files.

---
+++ winforms-hotkeys/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace winforms_hotkeys
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F);
+            textBox1.Location = new Point(90, 124);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(285, 39);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Segoe UI", 11F);
+            label1.ForeColor = Color.Maroon;
+            label1.Location = new Point(92, 72);
+            label1.Name = "label1";
+            label1.Padding = new Padding(2);
+            label1.Size = new Size(273, 36);
+            label1.TabIndex = 1;
+            label1.Text = "Hit F9 to Populate Textbox";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(478, 244);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Main Form";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+    }
+}
---
+++ winforms-hotkeys/MainForm.cs
@@ -0,0 +1,48 @@
+
+namespace winforms_hotkeys
+{
+    public partial class MainForm : Form, IMessageFilter
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Application.AddMessageFilter(this);
+            Disposed += (sender, e) => Application.RemoveMessageFilter(this);
+
+            // Add the shortcut methods you need
+            Shortcuts = new Dictionary<Keys, Action>
+            {
+                { Keys.F9, OnKeyF9 },
+                { Keys.Control | Keys.C, OnExit },
+            };
+        }
+
+        private void OnExit()
+        {
+            if(DialogResult.OK == MessageBox.Show("Application is exiting!"))
+            {
+                Close();
+            }
+        }
+
+        private Dictionary<Keys, Action> Shortcuts { get; }
+
+        private void OnKeyF9()
+        {
+            textBox1.Text = $"{nameof(OnKeyF9)} Detected!";
+        }
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == 0x100)
+            {
+                Keys keyData = (Keys)(int)m.WParam | Control.ModifierKeys;
+                if (Shortcuts.TryGetValue(keyData, out var action))
+                {
+                    action();
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
